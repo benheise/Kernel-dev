9546172b1a3e8a78885ec894773408e7d14ba202
FULLSHADE <54753063+FULLSHADE@users.noreply.github.com>
2021-11-28 21:00:47 -0500

    Update driver.cpp

--- getInfoDrvIoctl/driver/driver.cpp
+++ getInfoDrvIoctl/driver/driver.cpp
@@ -9,7 +9,7 @@ DRIVER_DISPATCH HandleIoctls;
 
 VOID DriverUnload(PDRIVER_OBJECT DriverObject)
 {
-    DbgPrint("Driver has been unloaded");
+    KdPrint(("Driver has been unloaded\n"));
 
     IoDeleteDevice(DriverObject->DeviceObject);
     IoDeleteSymbolicLink(&SYM_NAME);
@@ -20,7 +20,7 @@ NTSTATUS HandleIoctls(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 
     UNREFERENCED_PARAMETER(DeviceObject);
 
-    PIO_STACK_LOCATION stack_location;
+    PIO_STACK_LOCATION stack_location = NULL;
     NTSTATUS status = STATUS_SUCCESS;
 
     stack_location = IoGetCurrentIrpStackLocation(Irp);
@@ -29,21 +29,21 @@ NTSTATUS HandleIoctls(PDEVICE_OBJECT DeviceObject, PIRP Irp)
     {
     case IRP_MJ_DEVICE_CONTROL:
 
-        DbgPrint("IOCTL was recieved in kernel-mode");
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "IOCTL was recieved in kernel-mode", 0);
 
         if (stack_location->Parameters.DeviceIoControl.IoControlCode == INFO_IOCTL)
         {
-            DbgPrint("Hit IOCTL call");
+            DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Hit IOCTL call", 0);
         }
 
     case IRP_MJ_CREATE:
 
-        DbgPrint("Handle to the symolic link was created");
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Handle to the symolic link was created", 0);
         status = STATUS_SUCCESS;
 
     case IRP_MJ_CLOSE:
 
-        DbgPrint("Handle to the symolic link was closed");
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Handle to the symolic link was closed", 0);
         status = STATUS_SUCCESS;
 
     case IRP_MJ_WRITE:
@@ -62,12 +62,6 @@ NTSTATUS HandleIoctls(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 extern "C"
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
-    // basic info
-    DbgPrint("DriverStart Address 0x%x", DriverObject->DriverStart);
-    DbgPrint("DriverSize 0x%d", DriverObject->DriverSize);
-    DbgPrint("DriverSection Address 0x%x", DriverObject->DriverSection);
-    DbgPrint("DriverUnload Address 0x%x", DriverObject->DriverUnload);
-
     NTSTATUS status = 0;
 
     UNREFERENCED_PARAMETER(RegistryPath);
@@ -85,16 +79,16 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
     IoCreateDevice(DriverObject, 0, &DEV_NAME, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &DriverObject->DeviceObject);
     if (!NT_SUCCESS(status))
     {
-        DbgPrint("Failed to create the device");
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Failed to create the device", 0);
     }
     else {
         status = IoCreateSymbolicLink(&SYM_NAME, &DEV_NAME);
         if (NT_SUCCESS(status))
         {
-            DbgPrint("Symbolic link successfully created");
+            DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Symbolic link successfully created", 0);
         }
         else {
-            DbgPrint("Symbolic link not created");
+            DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Symbolic link not created", 0);
         }
 
     }
